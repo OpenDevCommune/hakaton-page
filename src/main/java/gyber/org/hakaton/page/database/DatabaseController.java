ab3e612c6db9adc186ff8f170bf5ad7b603f52fb
Nicki-afk <casperplay45@gmail.com>
2023-11-26 11:31:57 +0200

    write method getApplication()

--- src/main/java/gyber/org/hakaton/page/database/DatabaseController.java
+++ src/main/java/gyber/org/hakaton/page/database/DatabaseController.java
@@ -5,13 +5,23 @@ import gyber.org.hakaton.page.profile.ApplicationForParticipation;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Controller;
+
+import java.time.Instant;
+import java.time.ZoneId;
 
 @Controller
 public class DatabaseController {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
 
 
 
@@ -26,7 +36,7 @@ public class DatabaseController {
                         "    email_user VARCHAR(255) UNIQUE,\n" +
                         "    country VARCHAR(255),\n" +
                         "    about_me TEXT,\n" +
-                        "    date_sent_from TIMESTAMP DEFAULT CURRENT_TIMESTAMP\n" +
+                        "    date_sent_form TIMESTAMP DEFAULT CURRENT_TIMESTAMP\n" +
                         ");");
 
     }
@@ -48,6 +58,22 @@ public class DatabaseController {
 
 
     public boolean getApplication(Long applicationId){
+
+      ApplicationForParticipation getApp =
+        jdbcTemplate.query(
+                "SELECT id , date_sent_form ,  email_user , name_user , country , about_me FROM applications WHERE id = ?" ,
+                (rs , rowNum) -> new ApplicationForParticipation(
+                        rs.getLong("id") ,
+                        Instant.ofEpochMilli(rs.getDate("date_sent_form").getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                        rs.getString("name_user") ,
+                        rs.getString("email_user") ,
+                        rs.getString("about_me") ,
+                        rs.getString("country")) ,
+                applicationId
+        ).get(0);
+
+
+
         return false;
     }
 
