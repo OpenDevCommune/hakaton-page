5b47f1582ba420cc1d8f2446b66b2337cb2359c5
Nicki-afk <casperplay45@gmail.com>
2023-11-26 19:01:38 +0200

    rewrite javaScript code

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -7,19 +7,19 @@ const button = document.querySelector('button.submit_btn');
 button.addEventListener('click', () => {
 
 
-   if (xhr.readyState === 4 && xhr.status === 200) {
-      // Получаем ответ от сервера
-      const response = xhr.responseText;
-
-      // Обрабатываем ответ от сервера
-      // В этом случае мы просто выводим ответ в браузер
-      document.querySelector("body").innerHTML = response;
-   }
-
-   // Настраиваем параметры запроса
-   xhr.open("GET", "http://localhost:8080/form");
-
-   // Отправляем запрос
-   xhr.send();
+   fetch("http://localhost:8080/form", {
+     responseType: "text",
+   })
+     .then((response) => {
+       if (response.ok) {
+         // Успешный запрос
+         document.write(response.text);
+       } else {
+         // Ошибка запроса
+       }
+     })
+     .catch((error) => {
+       // Ошибка запроса
+     });
 
 });
