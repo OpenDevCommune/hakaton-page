36111cd06ea22a1ec50500e0262461763884cffc
Kostreic <kostrovalexandru@gmail.com>
2023-11-26 16:12:16 +0200

    Add class RegistrationFieldsValidator and implement standard methods

--- src/main/java/gyber/org/hakaton/page/validation/RegistrationFieldsValidator.java
+++ src/main/java/gyber/org/hakaton/page/validation/RegistrationFieldsValidator.java
@@ -1,4 +1,16 @@
 package gyber.org.hakaton.page.validation;
 
-public class RegistrationFieldsValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class RegistrationFieldsValidator implements ConstraintValidator<ValidateRegistrationFields, String> {
+    @Override
+    public void initialize(ValidateRegistrationFields constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return false;
+    }
 }
