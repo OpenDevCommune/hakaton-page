594e5ff91a70779db6262a7bd781d45a42fd077f
Nicki-afk <casperplay45@gmail.com>
2023-11-28 10:02:19 +0200

    added simple error controller

---
+++ src/main/java/gyber/org/hakaton/page/rest/ErrorRestController.java
@@ -0,0 +1,22 @@
+package gyber.org.hakaton.page.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorRestController {
+
+
+    @GetMapping("/err")
+    public String getErrorMessage(Model model){
+
+        model.addAttribute("errorText" , "Error . A user with such data is already registered. \nEnter other information or check your email");
+
+        return "application";
+
+    }
+}
