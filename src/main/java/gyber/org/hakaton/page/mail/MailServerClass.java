261d65241b072caaecdf6e281560280287b9e1db
Nicki-afk <casperplay45@gmail.com>
2023-12-17 19:59:52 +0200

    delete imports

--- src/main/java/gyber/org/hakaton/page/mail/MailServerClass.java
+++ src/main/java/gyber/org/hakaton/page/mail/MailServerClass.java
@@ -1,15 +1,12 @@
 package gyber.org.hakaton.page.mail;
 
-import jakarta.annotation.PostConstruct;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
