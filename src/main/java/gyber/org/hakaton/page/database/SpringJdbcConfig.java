a27257420d9d43015b92df3b27b05407d33d8da2
Nicki-afk <casperplay45@gmail.com>
2023-12-17 11:12:17 +0200

    added 2 profiles

--- src/main/java/gyber/org/hakaton/page/database/SpringJdbcConfig.java
+++ src/main/java/gyber/org/hakaton/page/database/SpringJdbcConfig.java
@@ -1,16 +1,53 @@
 package gyber.org.hakaton.page.database;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
+@Profile("prod")
 @Configuration
 public class SpringJdbcConfig {
 
+
+    private  String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+
+    @Value("${DATABASE_URL}")
+    private  String DATABASE_URL;
+
+    @Value("${DATABASE_PASS}")
+    private String DATABASE_PASS;
+
+
+    @Value("${DATABASE_USR}")
+    private String DATABASE_USR;
+
+    @Bean
+    public DataSource dataSource(){
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DATABASE_DRIVER);
+        dataSource.setUrl(DATABASE_URL);
+        dataSource.setPassword(DATABASE_PASS);
+        dataSource.setUsername(DATABASE_USR);
+
+        return dataSource;
+    }
+
+
+
+}
+
+@Profile("dev")
+@Configuration
+class SpringTestJdbcConfig{
 
     @Bean
     public DataSource dataSource(){
@@ -18,4 +55,5 @@ public class SpringJdbcConfig {
                 .setType(EmbeddedDatabaseType.H2)
                 .build();
     }
+
 }
