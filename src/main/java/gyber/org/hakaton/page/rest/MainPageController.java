5d9b2833780b91d7e1651d76aa7ba1cf8a5390bd
Nicki-afk <casperplay45@gmail.com>
2023-12-18 19:34:06 +0200

    added response params

--- src/main/java/gyber/org/hakaton/page/rest/MainPageController.java
+++ src/main/java/gyber/org/hakaton/page/rest/MainPageController.java
@@ -1,9 +1,12 @@
 package gyber.org.hakaton.page.rest;
 
-import gyber.org.hakaton.page.database.DatabaseController;
+import gyber.org.hakaton.page.database.DatabaseControllerDev;
 import gyber.org.hakaton.page.mail.MailServerClass;
 import gyber.org.hakaton.page.profile.ApplicationForParticipation;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +21,7 @@ public class MainPageController {
 
 
     @Autowired
-    private DatabaseController databaseController;
+    private DatabaseControllerDev databaseControllerDev;
 
     @Autowired
     private MailServerClass mailServerClass;
@@ -58,19 +61,35 @@ public class MainPageController {
             @RequestParam("email")  String email ,
             @RequestParam("fullName")   String fullName ,
             @RequestParam("aboutMe") String aboutMe ,
-            @RequestParam("country")  String country , Model model
+            @RequestParam("country")  String country ,
+            HttpServletResponse response
     ){
 
         logger.info("POST IN '/application/submit'. CREATE APPLICATION ... ");
 
         ApplicationForParticipation app = new ApplicationForParticipation(fullName , email , aboutMe , country);
 
+        logger.info("CHECKING IF THE USER HAS SUBMITTED AN APPLICATION PREVIOUSLY ... ");
+
+        if (databaseControllerDev.applicationExist(email)){
+            logger.info("USER ALREADY EXIST. REPEATED APPLICATION IS DENIED");
+
+            response.setStatus(409);
+            response.setHeader("Server-Response" , "A user with this email has already been registered before; 409 Conflict;");
+            return "success";
+
+        }
+
+        logger.info("USER APPLICATION IS NEW. PROCESS APPLICATION ... ");
+
         logger.info("APPLICATION CREATE SUCCESSFUL. PASSING CONTROL TO THE DATABASE MODULE...");
-        boolean result = databaseController.saveApplication(app);
+        boolean result = databaseControllerDev.saveApplication(app);
 
         if (!result) {
            logger.log(Level.SEVERE , "ERROR SAVE APPLICATION IN DATABASE. RETURN error_page");
 
+            response.setStatus(400);
+            response.setHeader("Server-Response" , "Error while saving application; Examine the server logs for additional information;");
             return "error_page";
         }
         else {
@@ -80,8 +99,10 @@ public class MainPageController {
 
             mailServerClass.sendMail(email, fullName);
 
+            response.setHeader("Server-Response" , "Application save successful;");
 
             return "success";
+
         }
 
 
